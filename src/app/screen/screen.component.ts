a055efdaff7c5001527750b51b833c622400bc48
Xing Jing <kom_xing_jing@simtech.a-star.edu.sg>
2022-08-12 07:31:20 +0800

    Add info about intents

--- src/app/screen/screen.component.ts
+++ src/app/screen/screen.component.ts
@@ -12,6 +12,7 @@ export class ScreenComponent implements OnInit {
   curr_state: string;
   next_states: Array<string>;
   entry_states: Array<string>;
+  screen_info: Map<string, string>;
 
   constructor(public stateService: StateServiceService) {
     this.state_space = new Map<string, Array<string>>()
@@ -20,18 +21,27 @@ export class ScreenComponent implements OnInit {
       .set("scent", ["holder colour", "error"])
       .set("holder colour", ["message", "error"])
       .set("message", ["cart review", "error"])
-      .set("cart review", ["product_edit", "scent_edit", "holder_colour_edit", "error", "complete"])
+      .set("cart review", ["product edit", "scent edit", "holder colour edit", "error", "complete"])
       .set("complete", []);
     this.curr_state = "order";
     this.next_states = this.state_space.get(this.curr_state) ?? [];
     this.entry_states = [...this.state_space.keys()]
+
+    this.screen_info = new Map<string, string>()
+    this.screen_info.set("order", "Choices: E-Scentz, MFConnect \n\n Intents: set_product")
+      .set("product", "Choices: E-Scentz, Refill \n\n Intents: set_product_type")
+      .set("scent", "Choices: Lavender, Eucalyptus, Tea tree, Peppermint, Citronella \n\n Intents: set_scent")
+      .set("holder colour", "Choices: White, Green, Blue \n\n Intents: set_holder_colour")
+      .set("message", "Choices: 1. Greetings from SIMTech!, 2. <no message> \n\n Intents: set_message")
+      .set("cart review", "to be discussed in meeting")
+      .set("complete", "")
   }
 
   ngOnInit(): void {
   }
 
   goto(state_name: string): void {
-    if (!["error", "product_edit", "scent_edit", "holder_colour_edit"].includes(state_name)) {
+    if (!["error", "product edit", "scent edit", "holder colour edit"].includes(state_name)) {
       this.next_states = this.state_space.get(state_name) ?? [];
     } else {
       this.next_states = [this.curr_state];
